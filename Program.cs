47c023d9289a7ee7de0b27801f4bf22a1dfd714e
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-13 10:24:59 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _11.doWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 11;
+            
+            do
+            {
+                i--;
+                Console.WriteLine("Hi, i aways enter here at least one time" +
+                    "and dont check the condition fist time");
+                Console.WriteLine(i);
+                
+                
+            } 
+            while (i >= 10);   // aways is   true i >= 10; infinite cycle;
+
+
+
+
+        }
+    }
+}
